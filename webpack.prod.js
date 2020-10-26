49f56c16851a590d662965df3d88d44ab03cb36e
indra <indra.anto42@gmail.com>
2020-10-26 18:51:53 +0700

    first commit

---
+++ webpack.prod.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+     
+    ],
+  },
+});
